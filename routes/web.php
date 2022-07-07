6b7244190427486dc05834433c9f9e725cebbe0b
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-07 18:06:12 +0200

    add php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('HomePage');
+    $comics = config('comics');
+    return view('HomePage', compact('comics'));
 });
