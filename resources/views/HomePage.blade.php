c53625748b8cf840bff5224267504d3ded93ca3d
Nothung429 <frank.gatto96.fg@gmail.com>
2022-07-07 16:37:23 +0200

    add

---
+++ resources/views/HomePage.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+        <title>DC Comix</title>
+    </head>
+    
+    <body>
+        <header>
+            <div class="ad">
+                <p>DC POWER VISA</p>
+                <p>ADDICIONAL DC SITES <></p>
+            </div>
+            <div class="header-core">
+                <div class="header-logo">
+                    <img src="{{asset('img/dc-logo.png')}}" alt="">
+                </div>
+                <div class="header-nav">
+                    <nav>
+                        <ul>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                            <li><a href="">home</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div class="header_search"></div>
+            </div>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+    </body>
+</html>
